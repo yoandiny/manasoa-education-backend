0192828326c4143210a583e2526fdd06b41ac312
Yoan <yoandiny14@gmail.com>
2025-05-08 23:21:31 +0300

    git protection fixed

--- bdd.js
+++ bdd.js
@@ -1,25 +1,25 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+// const { Pool } = require('pg');
+// require('dotenv').config();
 
-const dbConfig = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  certificate: fs.readFileSync(process.env.DB_CERT),
-};
+// const dbConfig = {
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_NAME,
+//   certificate: fs.readFileSync(process.env.DB_CERT),
+// };
 
-const pool = new Pool({
-  user: dbConfig.user,
-  password: dbConfig.password,
-  host: dbConfig.host,
-  port: dbConfig.port,
-  database: dbConfig.database,
-  ssl: {
-        rejectUnauthorized: true,
-        ca: dbConfig.certificate,
-    },
-});
+// const pool = new Pool({
+//   user: dbConfig.user,
+//   password: dbConfig.password,
+//   host: dbConfig.host,
+//   port: dbConfig.port,
+//   database: dbConfig.database,
+//   ssl: {
+//         rejectUnauthorized: true,
+//         ca: dbConfig.certificate,
+//     },
+// });
 
-module.exports = pool;
+// module.exports = pool;
