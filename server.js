0eb5c21001a0570b815996740c586ea914acd1ce
Yoan <yoandiny14@gmail.com>
2025-12-03 11:39:27 +0300

    🐛 fix(server): update SQL insertion for grade to include type_note_id in conflict handling

--- server.js
+++ server.js
@@ -503,7 +503,7 @@ app.post('/grade', async (req, res) => {
       const res = await pool.query(
         `INSERT INTO grade (student_id, subject_id, quarter_id, type_note_id, grade) 
 VALUES ($1, $2, $3, $4, $5) 
-ON CONFLICT (student_id, subject_id, quarter_id) 
+ON CONFLICT (student_id, subject_id, type_note_id, quarter_id) 
 DO UPDATE 
 SET grade = EXCLUDED.grade 
 RETURNING *`,
